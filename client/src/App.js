5b2d0b31bfc0f4fcfe1a9715937ff794403760d9
Jake Wagner <jkwagneriii@gmail.com>
2021-01-16 22:15:14 -0600

    one rotating cube

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+import './App.scss';
+import { Canvas, useFrame } from 'react-three-fiber'
+
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01 ))
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas>
+        <Box/>
+      </Canvas>
+    </>
   );
 }
 
