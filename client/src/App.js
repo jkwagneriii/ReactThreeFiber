a47c21add26e5d32115d49016669d1bdc2181bcf
Jake Wagner <jkwagneriii@gmail.com>
2021-01-16 22:47:33 -0600

    created floor

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import './App.scss';
 import { Canvas, useFrame } from 'react-three-fiber'
 
 
-const SpinningMesh = ({position}) => {
+const SpinningMesh = ({position, args, color}) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01 ))
   return (
     <mesh position={position} ref={mesh}>
-      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-      <meshStandardMaterial attach='material' color='#D1F7FF' />
+      <boxBufferGeometry attach='geometry' args={args} />
+      <meshStandardMaterial attach='material' color={color} />
     </mesh>
   )
 }
@@ -19,9 +19,31 @@ function App() {
     <>
       <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3}/>
-        <SpinningMesh position={[0, 1, 0]}/>
-        <SpinningMesh position={[-2, 1, -5]}/>
-        <SpinningMesh position={[5, 1, -2]}/>
+        <directionalLight 
+        position={[0, 10, 0]} 
+        intensity={1}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+        <pointLight position={[0, -10, 0]} intensity={1.5}/>
+
+        <group>
+          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+            <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+            <meshStandardMaterial attach='material' color="#D1FFD9" />
+          </mesh>
+        </group>
+
+
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="#D1F7FF"/>
+        <SpinningMesh position={[-2, 1, -5]} color="#ffd1f7"/>
+        <SpinningMesh position={[5, 1, -2]} color="#ffd1f7"/>
       </Canvas>
     </>
   );
